8416b48f8bb3899c140c74af5ce20e7429464b86
khankaif4877-ux <khankaif7748@gmail.com>
2026-01-14 11:33:36 +0530

    New commit

--- Java_Programs/src/practice/PracticeClass.java
+++ Java_Programs/src/practice/PracticeClass.java
@@ -6,7 +6,7 @@ public void testMethodTest() {
 	System.out.println("Hi");
 	System.out.println("Hi2");
 	System.out.println("Hi3");
-System.out.println("newchng");
+    System.out.println("newchng");
 	System.out.println("TL update");
 }
 }
